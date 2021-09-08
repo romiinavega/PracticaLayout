14d08ef7af1082f32d19e3a904e971bcbfaf1715
romiinavega <53828146+romiinavega@users.noreply.github.com>
2021-09-07 17:32:10 -0700

    Add files via upload

---
+++ PracticaLayout/PracticaLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticaLayout
+//
+//  Created by Alumno on 9/7/21.
+//  Copyright © 2021 Estudiate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
